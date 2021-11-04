fba8e8b71a5e99ba571a4a6c5692f74962a3c9db
Vitaly Turovsky <vital2580@icloud.com>
2021-11-04 17:34:04 +0300

    output both

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import { execSync } from 'child_process'
 import { endGroup, startGroup } from '@actions/core'
 import execa from 'execa'
 startGroup('output')
-execSync('pnpm -v')
-await execa('pnpm', ['-v'])
-console.log(process.env.PATH.split(':'))
+console.log(execSync('pnpm -v'))
+console.log(await execa('pnpm', ['-v']))
+// console.log(process.env.PATH.split(':'))
 endGroup()
