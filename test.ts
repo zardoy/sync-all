f61c1672124e1e957ffdc9181d9f623c5639d62e
Vitaly Turovsky <vital2580@icloud.com>
2021-11-04 17:38:54 +0300

    only execa

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process'
 import execa from 'execa'
-execSync('pnpm -v', { stdio: 'inherit' })
+// execSync('pnpm -v', { stdio: 'inherit' })
 await execa('pnpm', ['-v'], { stdin: 'inherit' })
 // console.log(process.env.PATH.split(':'))
