cf66d26c164b42de5825b4a07a25158f2826ed16
Vitaly Turovsky <vital2580@icloud.com>
2021-11-04 17:39:56 +0300

    test

--- test.ts
+++ test.ts
@@ -1,5 +1,6 @@
 import { execSync } from 'child_process'
 import execa from 'execa'
 // execSync('pnpm -v', { stdio: 'inherit' })
-await execa('pnpm', ['-v'], { stdin: 'inherit' })
+await execa('pnpm', ['i', '-g', 'vsce'], { stdio: 'inherit' })
+await execa('vsce', ['-v'], { stdio: 'inherit' })
 // console.log(process.env.PATH.split(':'))
