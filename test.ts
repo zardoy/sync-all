2516b52fdd9ea75dd30bd677ef5c0ca4455d076d
Vitaly Turovsky <vital2580@icloud.com>
2021-11-04 17:36:34 +0300

    into two steps

--- test.ts
+++ test.ts
@@ -1,8 +1,5 @@
 import { execSync } from 'child_process'
-import { endGroup, startGroup } from '@actions/core'
 import execa from 'execa'
-startGroup('output')
 execSync('pnpm -v', { stdio: 'inherit' })
 await execa('pnpm', ['-v'], { stdin: 'inherit' })
 // console.log(process.env.PATH.split(':'))
-endGroup()
